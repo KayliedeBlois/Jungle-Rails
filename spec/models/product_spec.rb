fb4e8fcb84ee880b1de405ac8cccf9ff15572dc9
KayliedeBlois <kayliedeblois@gmail.com>
2022-08-30 22:49:45 -0400

    Write product spec tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,42 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
   describe 'Validations' do
     # validation tests/examples here
 
-    before do
-      @product = Product.new
-      @product.valid?
+      before do
+        @product = Product.new
+        @product.valid?
 
-      @category = Category.new()
-      @category.name = "TestCategory"
+        @category = Category.new()
+        @category.name = "TestCategory"
 
-      @correctProduct = Product.new()
-      @correctProduct.name = "testName"
-      @correctProduct.price = 100
-      @correctProduct.quantity = 99 
-      @correctProduct.category = @category
-  end
+        @correctProduct = Product.new()
+        @correctProduct.name = "testName"
+        @correctProduct.price = 10
+        @correctProduct.quantity = 20 
+        @correctProduct.category = @category
+    end
 
-  it "Should not be valid when creating a product with no name" do
+    it "Should not be valid when creating a product with no name" do
 
-    nameMessage = @product.errors[:name][0]
-    expect(nameMessage).to eq("cannot leave name field empty")
-    expect(@correctProduct.errors.full_messages).to be_empty
+      nameMessage = @product.errors[:name][0]
+      expect(nameMessage).to eq("cannot leave name field empty")
+      expect(@correctProduct.errors.full_messages).to be_empty
+    end 
+
+    it "Should not be valid when creating a product with no price" do
+
+      nameMessage = @product.errors[:price][0]
+      expect(nameMessage).to eq("cannot leave price field empty")
+      expect(@correctProduct.errors.full_messages).to be_empty
+    end
+
+    it "Should not be valid when creating a product with no quantity" do
+      
+      nameMessage = @product.errors[:quantity][0]
+      expect(nameMessage).to eq("cannot leave quantity field empty")
+      expect(@correctProduct.errors.full_messages).to be_empty
+    end
+
+    it "Should not be valid when creating a product with no category" do
+
+      nameMessage = @product.errors[:category][0]
+      expect(nameMessage).to eq("cannot leave category field empty")
+      expect(@correctProduct.errors.full_messages).to be_empty
+    end
   end 
-
-  it "Should not be valid when creating a product with no price" do
-
-    nameMessage = @product.errors[:price][0]
-    expect(nameMessage).to eq("cannot leave price field empty")
-  end
-
-  it "Should not be valid when creating a product with no quantity" do
-    
-  end
-
-  it "Should not be valid when creating a product with no category" do
-
-  end
-  
 end
